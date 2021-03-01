40680bbe6c0e63775e4e83622fada87929be5411
Anzor Bashkhaz <anzorb@gmail.com>
2021-03-01 14:18:14 -0500

    SDK version Bump + find location from center

--- typescript-example/src/map.tsx
+++ typescript-example/src/map.tsx
@@ -1,19 +1,22 @@
-import React, { useContext, useEffect } from 'react';
-import { MiMapView, TMiMapViewOptions } from '@mappedin/react-native-sdk';
-import { RootContext, APPSTATE } from './app';
+import React, {useContext, useEffect} from 'react';
+import {MiMapView, TMiMapViewOptions} from '@mappedin/react-native-sdk';
+import {RootContext, APPSTATE} from './app';
+import {View} from 'react-native';
 
-export const Map = ({ options }: { options: TMiMapViewOptions }) => {
+export const Map = ({options}: {options: TMiMapViewOptions}) => {
   const {
     mapView,
     setVenue,
     directions,
     selectedMapId,
+    setMapState,
     setSelectedMapId,
     setCurrentLevel,
     setSelectedLocation,
     setNearestLocation,
     venueData,
     reset,
+    setMapDimensions,
     setAppState,
     setLoading,
   } = useContext(RootContext);
@@ -21,8 +24,6 @@ export const Map = ({ options }: { options: TMiMapViewOptions }) => {
   useEffect(() => {
     async function setMap() {
       if (directions != null) {
-        setLoading(true);
-
         await mapView.current?.focusOn({
           // @ts-ignore
           nodes: directions.path
@@ -41,8 +42,6 @@ export const Map = ({ options }: { options: TMiMapViewOptions }) => {
         selectedMapId != null &&
         mapView.current?.currentMap?.id !== selectedMapId
       ) {
-        setLoading(true);
-
         if (mapView.current?.currentMap?.id !== selectedMapId) {
           await mapView.current?.setMap(selectedMapId);
         }
@@ -54,74 +53,83 @@ export const Map = ({ options }: { options: TMiMapViewOptions }) => {
           });
         }
       }
-      setLoading(false);
     }
     setMap();
   }, [selectedMapId]);
 
   return (
-    <MiMapView
-      style={{ flex: 1 }}
-      key="mappedin"
-      ref={(ctrl) => {
-        if (ctrl != null) {
-          mapView.current = ctrl;
-        }
-      }}
-      onDataLoaded={({ venueData }) => {
-        setVenue(venueData);
-        setSelectedMapId(mapView.current?.currentMap?.id);
-      }}
-      onMapChanged={({ map }) => {
-        setSelectedMapId(map.id);
-      }}
-      onFirstMapLoaded={() => {
-        setLoading(false);
-      }}
-      options={options}
-      onBlueDotUpdated={({ update }) => {
-        const nearestNode = update.nearestNode;
-        if (nearestNode != null) {
-          const primaryLocation = nearestNode.locations[0];
-          if (primaryLocation != null) {
-            setNearestLocation(primaryLocation);
+    <View
+      style={{flex: 1}}
+      onLayout={(event) => {
+        const {width, height} = event.nativeEvent.layout;
+        setMapDimensions({width, height});
+      }}>
+      <MiMapView
+        style={{flex: 1}}
+        key="mappedin"
+        ref={(ctrl) => {
+          if (ctrl != null) {
+            mapView.current = ctrl;
           }
-        }
-        if (update.map != null) {
-          setCurrentLevel(update.map.name);
-        }
-      }}
-      onNothingClicked={reset}
-      onPolygonClicked={({ polygon }) => {
-        setAppState(APPSTATE.PROFILE);
-        mapView.current?.setSafeArea({
-          top: 0,
-          bottom: 450,
-          left: 0,
-          right: 0,
-        });
-        const location = venueData!.locations.find((l) =>
-          l.polygons.map((p) => p.id).includes(polygon.id),
-        );
-
-        if (location != null) {
-          setSelectedLocation(location);
-        }
-        mapView.current?.clearAllPolygonColors();
-        mapView.current?.setPolygonColor(polygon.id, 'red');
-        mapView.current!.focusOn({
-          polygons: [polygon.id],
-          minZoom: 2500,
-          tilt: 0.2,
-          // changeZoom: false,
-          padding: {
+        }}
+        onDataLoaded={({venueData}) => {
+          setVenue(venueData);
+          setSelectedMapId(mapView.current?.currentMap?.id);
+        }}
+        onMapChanged={({map}) => {
+          setSelectedMapId(map.id);
+        }}
+        onFirstMapLoaded={() => {
+          setLoading(false);
+        }}
+        onStateChanged={({state}) => {
+          setMapState(state);
+        }}
+        options={options}
+        onBlueDotUpdated={({update}) => {
+          const nearestNode = update.nearestNode;
+          if (nearestNode != null) {
+            const primaryLocation = nearestNode.locations[0];
+            if (primaryLocation != null) {
+              setNearestLocation(primaryLocation);
+            }
+          }
+          if (update.map != null) {
+            setCurrentLevel(update.map.name);
+          }
+        }}
+        onNothingClicked={reset}
+        onPolygonClicked={({polygon}) => {
+          setAppState(APPSTATE.PROFILE);
+          mapView.current?.setSafeArea({
             top: 0,
+            bottom: 450,
             left: 0,
             right: 0,
-            bottom: 450,
-          },
-        });
-      }}
-    />
+          });
+          const location = venueData!.locations.find((l) =>
+            l.polygons.map((p) => p.id).includes(polygon.id),
+          );
+
+          if (location != null) {
+            setSelectedLocation(location);
+          }
+          mapView.current?.clearAllPolygonColors();
+          mapView.current?.setPolygonColor(polygon.id, 'red');
+          mapView.current!.focusOn({
+            polygons: [polygon.id],
+            minZoom: 2500,
+            tilt: 0.2,
+            // changeZoom: false,
+            padding: {
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 450,
+            },
+          });
+        }}
+      />
+    </View>
   );
 };
